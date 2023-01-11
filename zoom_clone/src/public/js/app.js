ef4fdf299780bbea84662267694330c417a191b3
Oliverslife <voise.ksing@gmail.com>
2023-01-12 01:21:03 +0900

    23.01.12

--- zoom_clone/src/public/js/app.js
+++ zoom_clone/src/public/js/app.js
@@ -1,1 +1,61 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✔");
+});
+
+socket.addEventListener("message", (message) => {
+    // console.log("New message : ", message);
+    // console.log("New message : ", JSON.parse(message)); 
+    console.log(message);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);   
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected to Server ❌");
+});
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    // console.log(message);
+    const li = document.createElement("li");
+    li.innerText = `You : ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+};
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
+
+
+
+
+
+
+
+
+
+
+// setTimeout(() => {
+//     socket.send("hello from the browser!");
+// }, 10000);
--- zoom_clone/src/server.js
+++ zoom_clone/src/server.js
@@ -1,5 +1,6 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-import { render } from "pug";
 
 const app = express();
 
@@ -10,4 +11,53 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);  //http Server
+const wss = new WebSocket.Server({ server });
+
+// function handleConnection(socket) {
+//     console.log(socket)
+// }
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    sockets["nickname"] = "Anon";
+    console.log("Connected to Browser ✔")
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        // console.log(parsed, message);
+
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.toString}`));
+                console.log(message.payload);
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                console.log(message.payload);
+        };
+
+        // if(message.type === "new_message") {
+        //     sockets.forEach((aSocket) => aSocket.send(message.payload));
+        // } else if(message.type === "nickname") {
+        //     console.log(message.payload);
+        // };
+
+        
+
+
+
+        // sockets.forEach((aSocket) => aSocket.send(JSON.stringify(message)));
+        // console.log(message);
+        // console.log(message.toString());
+        // console.log(message.toString("utf-8"));
+        // socket.send(message.toString("utf-8"));
+    });
+    // socket.send("hello!!!");
+})
+
+server.listen(3000, handleListen);
