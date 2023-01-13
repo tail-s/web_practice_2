28301f4c4412eaef38777cb54784ec82bfa7e622
Oliverslife <voise.ksing@gmail.com>
2023-01-14 02:58:22 +0900

    23.01.14

--- zoom_clone/src/public/js/app.js
+++ zoom_clone/src/public/js/app.js
@@ -15,9 +15,9 @@ function addMessage(message) {
     ul.appendChild(li);
 };
 
-function handleMEssageSubmit(event) {
+function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You: ${value}`);
@@ -25,13 +25,21 @@ function handleMEssageSubmit(event) {
     input.value = "";
 };
 
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+};
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMEssageSubmit);
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 };
 
 function handleRoomSubmit(event) {
@@ -50,12 +58,12 @@ form.addEventListener("submit", handleRoomSubmit);
 
 
 
-socket.on("welcome", () => {
-    addMessage("someone joined!");
+socket.on("welcome", (user) => {
+    addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", () => {
-    addMessage("someone left ㅜㅜ");
+socket.on("bye", (left) => {
+    addMessage(`${left} left ㅜㅜ`);
 });
 
 socket.on("new_message", addMessage);
--- zoom_clone/src/server.js
+++ zoom_clone/src/server.js
@@ -18,21 +18,23 @@ const server = http.createServer(app);  //http Server
 const io = SocketIO(server);
 
 io.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
     socket.onAny((event) => {
         console.log(`Socket Event:${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
     });
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     });
+    socket.on("nickname", (nickname) => socket["nickname"] = nickname)
     
     // socket.on("enter_room", (msg, done) => {
     //     console.log(msg);
