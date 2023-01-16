a2753994f2bd0c8d289b68ecc54ea5c410d48759
Oliverslife <voise.ksing@gmail.com>
2023-01-17 03:17:42 +0900

    video call start

---
+++ video_call/src/server.js
@@ -0,0 +1,17 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+server.listen(3000, handleListen);
