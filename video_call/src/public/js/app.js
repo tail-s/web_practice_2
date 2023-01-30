fc55c80d17cc215adca49db60de832bb8c82b2bf
Oliverslife <voise.ksing@gmail.com>
2023-01-31 03:16:12 +0900

    unchecked

--- video_call/src/public/js/app.js
+++ video_call/src/public/js/app.js
@@ -121,14 +121,22 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async(offer) => {
+    console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
 });
 
 socket.on("answer", answer => {
+    console.log("received the answer");
     myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", ice => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC Code
@@ -136,12 +144,19 @@ socket.on("answer", answer => {
 function makeConnection() {
     myPeerConnection = new RTCPeerConnection();
     myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream
     .getTracks()
-    .forEach(track => myPeerConnection.addTrack(track, myStream));
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
 };
 
 function handleIce(data) {
-    console.log("got ice candidate");
-    console.log(data);
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+};
+
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    console.log("Peer's Stream", data.stream);
+    peersStream.srcObject = data.stream;
 };
--- video_call/src/server.js
+++ video_call/src/server.js
@@ -23,6 +23,9 @@ wsServer.on("connection", socket => {
     });
     socket.on("answer", (answer, roomName) => {
         socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
     })
 });
 
